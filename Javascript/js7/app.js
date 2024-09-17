a0787fab50c45e4fe1406683d5d77d8b99dad9fd
ranjeetkumar4941 <ranjeetkumar49424@gmail.com>
2024-09-17 23:53:07 +0530

    js7 add without qs & Ans

---
+++ Javascript/js7/app.js
@@ -0,0 +1,72 @@
+// this keyword
+const student={
+    name: "Amit",
+    rollno: 687,
+    math: 56,
+    eng: 76,
+    phy: 89,
+    getAvg: function(){
+        console.log((this.math+this.eng+this.phy)/3);
+    }
+}
+student.getAvg(56,76,89);
+
+// try & catch
+console.log("Hello");
+console.log("Hello");
+try{
+    console.log(a);
+} catch(err) {
+    console.log(err);
+}
+console.log("Hello2");
+console.log("Hello2");
+
+// arrow function
+let add=(a,b)=>{
+    console.log(a+b);
+}
+add(3,6);
+
+let cube= n => {   // when we have only one parameter then paranthesis is not compulsory
+    return n**3;
+}
+console.log(cube(3));
+
+let pow= () => {      // when we have no parameter then we can keep empty our paranthesis
+    console.log(2**3);
+}
+pow();
+
+// implicit return in arrow function
+let mul= (a,b) => (     // when we just return value using function then we use it. 
+    a*b    
+);                      // in this method we use paranthesis not curlybresis
+console.log(mul(2,4));
+
+let addTwoNum= (a,b) => a+b;    // bracket is not compulsory when we just want to return value using arrow function
+console.log(addTwoNum(3,6));
+
+// set Timeout function
+console.log("Hii Buddy");
+setTimeout(() => {
+    console.log("this is set Timeout function.");
+    
+}, 3000);
+console.log("Welcome!");
+
+// set interval function
+// setInterval(() => {         // it will run infinite time
+//     console.log("Hello world");
+    
+// }, 2000);
+let id = setInterval(() => {    // it will not run infinite time because line 67
+    console.log("Ranjeet");
+    
+},2000); 
+clearInterval(id);
+
+
+
+
+
