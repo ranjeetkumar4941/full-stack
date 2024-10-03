33b8c4fff806b868c55c1e7556b7dbdfe1104111
ranjeetkumar4941 <ranjeetkumar49424@gmail.com>
2024-10-04 00:45:54 +0530

    Part completed

--- Javascript/js11/app.js
+++ Javascript/js11/app.js
@@ -68,3 +68,77 @@ saveToDB( // it is callback hell
     }
 );
  
+// promises
+function saveData(data){
+    return new Promise((resolve, reject) => { // resolve and reject will pass in every time when we return promise
+        let networkSpeed=Math.floor(Math.random()*10)+1;
+        if(networkSpeed > 4){
+            resolve("Success: solved problem");
+        }else{
+            reject("Failure: Not Solved problem");
+        }
+    })
+}
+// console.log(saveData("hello"));
+
+// then and catch
+saveData("Hello world")
+    .then(() => { //after success, if we want to do anything then we use .then which takes a callback
+        console.log("promise resolved");
+        
+    })
+    .catch(() => { // after failure, if we want to do anything then we use .catch which also takes a callback
+        console.log("promise rejected");
+        
+    });
+
+console.log("Promise Chaining");
+    
+// promise chaining 
+saveData("data1")
+    .then((result) => { // result is the value of resolve function's parenthesis 
+        console.log("data1 stored");
+        console.log(result);
+        
+        return saveData("data2"); // for chaining we return function call which works as a function call for next then
+    })
+    .then((result) => {
+        console.log("data2 stored");
+        console.log(result);
+        return saveData("data3");
+    })
+    .then((result) => {
+        console.log("data3 stored");
+        console.log(result);
+    })
+    .catch((error) => { // error is the value of reject function's parenthesis
+        console.log(error);
+        console.log("Remaining Data not stored");
+    })
+
+// creating color changer code like 'h1' but using 'promise' not 'callback hell'
+let h3=document.querySelector("h3");
+function h3ColorChanger(color, delay){
+    return new Promise((resolve,reject) => {
+        setTimeout(() => {
+            h3.style.color=color;
+            resolve(color);
+        }, delay);
+    })
+}
+h3ColorChanger("blue",1000)
+    .then((result) => {
+        console.log(result);
+        return h3ColorChanger("red", 1000);
+    })
+    .then((result) => {
+        console.log(result);
+        return h3ColorChanger("orange", 1000);
+    })
+    .then((result) => {
+        console.log(result);
+        return h3ColorChanger("brown", 1000);
+    })
+    .then((result) => {
+        console.log(result);
+    })
