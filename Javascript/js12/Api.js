a87dca1fca725e5d1fabfca350e4719107ac2c0c
ranjeetkumar4941 <ranjeetkumar49424@gmail.com>
2024-10-15 00:07:39 +0530

    Part completed

--- Javascript/js12/Api.js
+++ Javascript/js12/Api.js
@@ -16,3 +16,52 @@ console.log(student);
 // for change our js object into JSON format we use JSON.stingify(obj)
 let jsonStudent=JSON.stringify(student);
 console.log(jsonStudent); // it will print string because in js json is string
+
+// Api request 
+let api="http://catfact.ninja/fact";
+fetch(api) //we use it to fetch info from api
+    .then((response) => { //fetch returns promise so we use then to track it
+        // console.log(response);
+        return response.json();// to fetch actual data we use it and it also return promise
+    })
+    .then((result) => {
+        document.querySelector("#btn1").addEventListener("click",() => {
+            printFact1(result);
+        })
+        return fetch(api);
+    })
+    .then((response) => {
+        return response.json();
+    })
+    .then((result) => {
+        document.querySelector("#btn2").addEventListener("click",() => {
+            printFact2(result);
+        })
+    })
+    .catch((err) => {
+        console.log(err);
+        
+    })
+function printFact1(res){
+    let para = document.createElement("p");
+    para.innerText=res.fact;
+    document.querySelector("#fact1").insertAdjacentElement("afterend",para);
+}
+function printFact2(res){
+    let para = document.createElement("p");
+    para.innerText=res.fact;
+    document.querySelector("#fact2").insertAdjacentElement("afterend",para);
+}
+
+// fetch with async & await
+async function fetchData() {
+    try {
+        let response=await fetch(api);
+        let data=await response.json();
+        console.log(data);
+    } catch (error) {
+        console.log(error);
+        
+    }
+}
+fetchData();
