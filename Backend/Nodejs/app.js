585d19491f953f0f46766074b342647429a6d562
ranjeetkumar4941 <ranjeetkumar49424@gmail.com>
2024-10-20 01:26:11 +0530

    part completed

--- Backend/Nodejs/app.js
+++ Backend/Nodejs/app.js
@@ -1,8 +1,8 @@
 // module.exports=3234;
-let sum =(a,b) => a+b;
-let mul = (a,b) => a*b;
-const g=9.18;
-const PI=3.14;
+export let sum =(a,b) => a+b;
+export let mul = (a,b) => a*b;
+export const g=9.18;
+export const PI=3.14;
 
 const obj = {
     sum: sum,
@@ -11,5 +11,5 @@ const obj = {
     Pi: PI
 }
 // module.exports=obj;
-module.exports.g=9.18;
-module.exports.PI=3.14;
+// module.exports.g=9.18;
+// module.exports.PI=3.14;
--- Backend/Nodejs/script.js
+++ Backend/Nodejs/script.js
@@ -13,6 +13,14 @@
 // console.log(appFileData);
 
 // require Directory
-let directoryData=require("./jsFile"); // it track index file which is similar to main() in c/c++
-console.log(directoryData);
+// let directoryData=require("./jsFile"); // it track index file which is similar to main() in c/c++
+// console.log(directoryData);
 
+// Import : we can use either import or require
+// before use import we add a property in package.json file in same directory which is type with module value
+import{sum, mul} from "./app.js"; // we write file name with extension
+console.log(sum(3,6));
+console.log(mul(3,6));
+// generate random words
+import { generate, count } from "random-words";
+console.log(generate());
