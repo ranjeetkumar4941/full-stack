967362cfc7712741dffd86a8b1693387d61a1c64
ranjeetkumar4941 <ranjeetkumar49424@gmail.com>
2025-08-04 11:04:01 +0530

    after show detail section

--- Backend/5. Rest/index.js
+++ Backend/5. Rest/index.js
@@ -1,7 +1,10 @@
+const { log } = require('console');
 const express = require('express');
 const app = express();
 const path = require('path');
 const port = 3000;
+
+const { v4: uuidv4 } = require('uuid');
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
@@ -12,18 +15,22 @@ app.use(express.urlencoded({ extended: true }));
 
 let posts = [
     {
+        id: uuidv4(),
         Username: "John",
         content: "This is my first post",
     },
     {
+        id: uuidv4(),
         Username: "Jane",
         content: "Today is a great day!",
     },
     {
+        id: uuidv4(),
         Username: "Doe",
         content: "This is a sample post",
     },
     {
+        id: uuidv4(),
         Username: "Alice",
         content: "Learning Express.js is fun!", 
     }
@@ -38,9 +45,22 @@ app.get('/posts/new', (req, res) => {
 
 app.post('/posts', (req, res) => {
     let {Username, content} = req.body;
-    posts.push({ Username, content });
-    res.render('index.ejs', { posts });
+    let id = uuidv4(); //to create a random id, we use it.
+    posts.push({ id, Username, content });
+
+    res.redirect("/posts")
 });
+
+app.get('/posts/:id', (req, res) => {
+    let id = req.params.id;
+    let post = posts.find((p) => id === p.id);
+    if(post){
+        res.render("showPost.ejs", { post });
+    }else{
+        res.send("<h1>Enter a valid ID</h1>");
+    }
+});
+
 app.listen(port,()=>{
     console.log(`Server is running on http://localhost:${port}`);
 });
