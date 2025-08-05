87fa43c4fc0422bbef5c6ab28dc099157afc149b
ranjeetkumar4941 <ranjeetkumar49424@gmail.com>
2025-08-06 00:02:09 +0530

    completed

--- Backend/5. Rest/index.js
+++ Backend/5. Rest/index.js
@@ -4,6 +4,11 @@ const app = express();
 const path = require('path');
 const port = 3000;
 
+// this is npm package and used to override method because form doesn't accept PATCH, PUT and DELETE req, form in HTML only accepts post and get request. So, we override method like post & get using it. 
+const methodoverride = require("method-override");
+app.use(methodoverride("_method"));
+
+// it is npm package and used to create a random id for posts.
 const { v4: uuidv4 } = require('uuid');
 
 app.set('view engine', 'ejs');
@@ -13,9 +18,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(express.urlencoded({ extended: true }));
 
+// creating temporary posts for page as 'array of objects'
 let posts = [
     {
-        id: uuidv4(),
+        id: uuidv4(), // we call the function of uuid to generate a random ID.
         Username: "John",
         content: "This is my first post",
     },
@@ -35,10 +41,12 @@ let posts = [
         content: "Learning Express.js is fun!", 
     }
 ]
+// to get all post
 app.get('/posts', (req, res) => {
     res.render('index.ejs', { posts});
 });
 
+// to create a new post
 app.get('/posts/new', (req, res) => {
     res.render('newPost.ejs');
 });
@@ -47,10 +55,10 @@ app.post('/posts', (req, res) => {
     let {Username, content} = req.body;
     let id = uuidv4(); //to create a random id, we use it.
     posts.push({ id, Username, content });
-
-    res.redirect("/posts")
+    res.redirect("/posts"); // we use it to link pages. it sends "get" request over this path- "/posts"
 });
 
+// To get detail of a post
 app.get('/posts/:id', (req, res) => {
     let id = req.params.id;
     let post = posts.find((p) => id === p.id);
@@ -59,6 +67,28 @@ app.get('/posts/:id', (req, res) => {
     }else{
         res.send("<h1>Enter a valid ID</h1>");
     }
+});
+
+// To edit post
+app.get("/posts/:id/edit", (req, res) => {
+    let id = req.params.id;
+    let post = posts.find(p => p.id === id);
+    res.render("editpost.ejs", { post });
+});
+
+app.patch("/posts/:id", (req, res) => {
+    let id = req.params.id;
+    let content = req.body.content;
+    let post = posts.find(p => p.id === id);
+    post.content = content;
+    res.redirect("/posts");
+});
+
+// To delete post
+app.delete("/posts/:id", (req, res) => {
+    let id = req.params.id;
+    posts = posts.filter(p => p.id !== id);
+    res.redirect("/posts");
 });
 
 app.listen(port,()=>{
