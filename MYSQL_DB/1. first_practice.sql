479ec1a2c7c8ca12508b35d8bc9645cc9516b825
ranjeetkumar4941 <ranjeetkumar49424@gmail.com>
2025-08-07 22:50:14 +0530

    MYSQL start

---
+++ MYSQL_DB/1. first_practice.sql
@@ -0,0 +1,5 @@
+CREATE DATABASE college; -- we use it to create our database 
+USE college; -- we use it to tell that which database we are going to use. 
+create database company; -- SQL is not a Case Sensitive language 
+drop database CompaNy; -- use it delete database
+
