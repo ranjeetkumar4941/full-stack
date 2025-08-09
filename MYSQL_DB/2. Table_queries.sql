798b8db9bf05a7a682ec1eafe3d6c572c6e17684
ranjeetkumar4941 <ranjeetkumar49424@gmail.com>
2025-08-09 22:34:36 +0530

    till select command

--- MYSQL_DB/2. Table_queries.sql
+++ MYSQL_DB/2. Table_queries.sql
@@ -1,6 +1,7 @@
 CREATE DATABASE IF NOT EXISTS instagram;
 USE instagram;
 
+-- CREATE COMMAND: used for create table or DB
 -- constraints- unique, not null, ceck, default- set default value, if value doesn't exist
 CREATE TABLE user (
 Id INT UNIQUE, -- 'unique- column value should be unique.'
@@ -11,3 +12,16 @@ followers INT,
 following INT,
 CONSTRAINT CHECK (age>=18) -- sets limit for a column
 );
+
+-- INSERT COMMAND: used for insert data into table
+INSERT INTO user VALUES
+(1,23,"John","john@gmail.in",20,34),
+(2,19,"ketki","ketki@yahoo.com",343, 462),
+(3, 21, "prasad", "prasad@gmaol.in", 234,556);
+
+INSERT INTO user (id, age, name) VALUES -- We can insert value in a particular column but it should follow constraints 
+(4, 27, "Anurag");
+
+-- SELECT COMMAND: used for view our data
+SELECT * FROM user;-- it views total data of table
+SELECT id, name FROM user;-- it view only selected column data from table
