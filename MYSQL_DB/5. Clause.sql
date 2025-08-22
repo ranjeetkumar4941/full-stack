70b24bce039efb1bcd9c070b231d67518e4149eb
ranjeetkumar4941 <ranjeetkumar49424@gmail.com>
2025-08-22 22:32:49 +0530

    all clauses added

--- MYSQL_DB/5. Clause.sql
+++ MYSQL_DB/5. Clause.sql
@@ -42,3 +42,15 @@ order by name; -- by default it sorts in Ascending order
 select * from user 
 order by followers asc;
 
+-- Group By clause: It groups rows that have same values
+select age, max(followers) from user -- we can't write other columns name 
+group by age;
+
+select age, count(age) from user
+group by age;
+
+-- Having clause: it is similar to where for apllying condition but, only for groups and for it grouping are necessary.
+select age, max(followers) 
+from user
+group by age
+having max(followers) > 100;
