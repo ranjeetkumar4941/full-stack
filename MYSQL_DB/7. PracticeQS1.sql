afcdc5ddd2336f3f09c5858daae5df1a806395af
ranjeetkumar4941 <ranjeetkumar49424@gmail.com>
2025-08-24 00:09:29 +0530

    all practice Qs completed

--- MYSQL_DB/7. PracticeQS1.sql
+++ MYSQL_DB/7. PracticeQS1.sql
@@ -1,3 +1,5 @@
+-- QS1
+
 CREATE DATABASE IF NOT EXISTS college;
 USE college;
 
@@ -37,3 +39,55 @@ ADD COLUMN city VARCHAR(30) DEFAULT "Gurgaon";
 -- delete the salary column
 ALTER TABLE teacher 
 DROP COLUMN ctc;
+
+-- QS2
+
+CREATE TABLE student (
+roll_no INT,
+name VARCHAR(30),
+city VARCHAR(30),
+marks int
+);
+
+INSERT INTO student VALUES
+(110, "adam", "Delhi", 76),
+(108, "bob", "Mumbai", 65),
+(124, "casey", "Pune", 94),
+(112, "duke", "Pune", 80);
+
+SELECT * FROM student;
+-- select student who scored 75+
+SELECT * FROM student
+WHERE marks > 75;
+
+-- find all cities name
+SELECT DISTINCT city FROM student;
+
+SELECT city FROM student
+GROUP BY city;
+
+-- find maximum marks of students from each city
+SELECT city, MAX(marks) FROM student
+GROUP BY city;
+
+-- find the average of class
+SELECT AVG(marks) FROM student;
+
+-- add a new column grade and assign grade such that :
+-- above 80, grade O
+-- between 70-80, grade A
+-- between 70-60, grade B
+ALTER TABLE student
+ADD COLUMN grade VARCHAR(2);
+
+UPDATE student 
+SET grade = "O"
+WHERE marks > 80;
+
+UPDATE student 
+SET grade = "A"
+WHERE 80 >= marks AND marks >  70;
+
+UPDATE student 
+SET grade = "B"
+WHERE 70 >= marks AND marks > 60;
