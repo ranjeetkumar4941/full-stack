fc5366a26e53e7e29db8778bdf7c81f806249c56
ranjeetkumar4941 <ranjeetkumar49424@gmail.com>
2025-12-31 15:05:55 +0530

    more details added

--- Backend/6. Node with SQL/mysql_connection/connection.js
+++ Backend/6. Node with SQL/mysql_connection/connection.js
@@ -8,9 +8,17 @@ const connection = mysql.createConnection({
     password: '#Ranjeet4035'
 });
 
-connection.query('DROP TABLE temp', (err, results) => {
+// connection.query('DROP TABLE temp', (err, results) => {
+//     if(err) throw err;
+//     console.log(results);
+// });
+
+// we can also write our query in a variable
+let query = "DROP TABLE temp";
+connection.query(query, (err, result) => {
     if(err) throw err;
-    console.log(results);
-});
+    console.log(result); // it gives output an array of objects
+    
+})
 
 connection.end();
