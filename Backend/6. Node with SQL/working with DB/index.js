cc6da0de45a1ff034467da2c69bc8c3b066208c3
ranjeetkumar4941 <ranjeetkumar49424@gmail.com>
2026-01-06 16:44:45 +0530

    home, show user and add user route completed

---
+++ Backend/6. Node with SQL/working with DB/index.js
@@ -0,0 +1,85 @@
+// I AM USING COMPANY DB WHICH I CREATED IN OPERATION DIRECTORY 
+const { log } = require('console');
+const express = require('express');
+const app = express();
+const port = 8080;
+const path = require('path');
+const mysql = require('mysql2');
+const {v4 : uuidv4} = require('uuid');
+
+// setting view engine
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.urlencoded({ extended: true })); // to parse form data
+
+// creating connection with db
+const connection = mysql.createConnection({
+    user: "root",
+    host: "localhost",
+    database: "company",
+    password: "#Ranjeet4035"
+});
+
+// Home route
+app.get("/", (req, res) => {
+    try {
+        let q= "SELECT COUNT(*) AS COUNT FROM user"; // here, i renamed COUNT(*) AS COUNT.
+    connection.query(q, (err, result) => {
+        if(err) throw err;
+        let count = result[0].COUNT;
+        res.render('home.ejs', {count});
+        
+    });
+    } catch (error) {
+        console.log(error);
+        res.send("Error in DB.");
+    }
+});
+
+//show all user route
+app.get("/users", (req, res) => {
+    try {
+        let q = "SELECT id, username, email FROM user"; // we will not display password because it is sensitive information of a user
+        connection.query(q, (err, result) => {
+            if(err) throw err;
+            res.render('show_users.ejs',{result});
+            
+        });
+    } catch (error) {
+        console.log(error);
+        res.send("Error in DB.");
+    }
+    
+});
+
+// adding new user
+// UI for add user
+app.get("/user", (req, res) => {
+    res.render('newUser.ejs')
+});
+// store data in db
+app.post("/user", (req, res) => {
+    let data = [
+        uuidv4(),
+        req.body.username,
+        req.body.email,
+        req.body.password
+    ];
+    try {
+        let q="INSERT INTO user VALUES (?,?,?,?)";
+        connection.query(q, data, (err, result) => {
+        if(err) throw err;
+        // console.log(result);
+        res.redirect("/");
+    });
+    } catch (error) {
+        console.log(error);
+        res.send("Error in DB.");
+    }
+    
+    
+});
+app.listen(port, () => {
+    console.log(`App is listening at http://localhost:${8080}`);
+});
