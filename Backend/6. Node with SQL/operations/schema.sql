7d890697f9c6b6633a8e449c15b48be9beff1f19
ranjeetkumar4941 <ranjeetkumar49424@gmail.com>
2026-01-06 14:09:25 +0530

    all operation done

--- Backend/6. Node with SQL/operations/schema.sql
+++ Backend/6. Node with SQL/operations/schema.sql
@@ -1,6 +1,6 @@
 -- If we have more query then we used sql files to write our queries
 CREATE TABLE IF NOT EXISTS user(
-    id INT PRIMARY KEY,
+    id varchar(100) PRIMARY KEY,
     username VARCHAR(50) UNIQUE,
     email VARCHAR(100) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL
--- Backend/6. Node with SQL/operations/script.js
+++ Backend/6. Node with SQL/operations/script.js
@@ -1,4 +1,5 @@
 const mysql = require('mysql2');
+const { faker } = require('@faker-js/faker');
 
 const connection = mysql.createConnection({
     user : "root",
@@ -7,43 +8,68 @@ const connection = mysql.createConnection({
     password : "#Ranjeet4035"
 });
 
-try{
-    connection.query("SHOW TABLES", (err, result) => {
-        if(err) throw err;
-        console.log(result);
-    });
+// try{
+//     connection.query("SHOW TABLES", (err, result) => {
+//         if(err) throw err;
+//         console.log(result);
+//     });
 
-    //Inserting data
-    // If we have to store a single row then we use this method
-    // let q = "INSERT INTO user (id, username, email, password) VALUES (?,?,?,?)";// we use placeholder(?) which replaces with value
-    // let user = [123, 'jack', 'abc@gmail.com', 'fd12df'];
-    // connection.query(q,user, (err, result) => {
-    //     if(err) throw err;
-    //     console.log(result);
+//     //Inserting data
+//     // If we have to store a single row then we use this method
+//     // let q = "INSERT INTO user (id, username, email, password) VALUES (?,?,?,?)";// we use placeholder(?) which replaces with value
+//     // let user = [123, 'jack', 'abc@gmail.com', 'fd12df'];
+//     // connection.query(q,user, (err, result) => {
+//     //     if(err) throw err;
+//     //     console.log(result);
         
-    // })
+//     // })
 
-    // if we have to insert multiple rows then we use array of arrays and single placeholder(?).
-    let query = "INSERT INTO user (id, username, email, password) VALUES ?";
-    let users = [
-        [123134, 'jack134', 'abc@gmail.com134', 'fd12df134'],
-        [123125, 'jack125', 'abc@gmail.com125', 'fd12df125']
+//     // if we have to insert multiple rows then we use array of arrays and single placeholder(?).
+//     let query = "INSERT INTO user (id, username, email, password) VALUES ?";
+//     let users = [
+//         [12313, 'jack13', 'abc@gmail.com13', 'fd12df13'],
+//         [12312, 'jack12', 'abc@gmail.com12', 'fd12df12']
 
-    ];
+//     ];
 
-    connection.query(query,[users], (err, result) => {
-        if(err) throw err;
-        console.log(result);
+//     connection.query(query,[users], (err, result) => {
+//         if(err) throw err;
+//         console.log(result);
         
-    })
+//     })
 
-    connection.query("SELECT * FROM user", (err, result) => {
-        if(err) throw err;
-        console.log(result);
-    });
-}catch(err){
-    console.log(err);
+//     connection.query("SELECT * FROM user", (err, result) => {
+//         if(err) throw err;
+//         console.log(result);
+//     });
+// }catch(err){
+//     console.log(err);
     
+// }
+
+// creating fake date and insert bulk data
+let user = () => { 
+    return [
+    faker.string.uuid(),
+    faker.internet.username(),
+    faker.internet.email(),
+    faker.internet.password()
+    ];
+};
+
+let usersData= [];
+for(let i=0; i<100; i++){
+    usersData[i] = user();
+}
+
+try {
+    let q = "INSERT INTO user VALUES ?";
+    connection.query(q, [usersData], (err, result) => {
+        if(err) throw err;
+        console.log(result);
+    });
+} catch (error) {
+    console.log(error);
 }
 
 connection.end();
