020f5a48b7579c40f4f69719d637f93699d6d78d
ranjeetkumar4941 <ranjeetkumar49424@gmail.com>
2026-01-09 21:56:16 +0530

    more detail added

--- MongoDB/1_create(insert).mongodb.js
+++ MongoDB/1_create(insert).mongodb.js
@@ -1,10 +1,15 @@
+// in MongoDB, we use camelcase for method name.
+// semicolon is not mendatory but it is a good practice to use it
+
 use("College"); //to use or create db. if db exist then it will use that otherwise it will create a new one
-db.dropDatabase();
+
+db.dropDatabase(); // delete database
+
 //If we doesn't insert data in this db it will delete from memory when we quit from mongoDB.
 // If we want to store our db in memory then we have to insert data in that database.
 
 // To insert data 
-// this method is used to insert a single document(similar to MYSQL row)
+// .insertOne() method is used to insert a single document(similar to MYSQL row), it takes an object in form of json data
 db.student.insertOne( //here, student is a collection(Similar to MYSQL table)
     {
         "name": "John",
@@ -12,8 +17,9 @@ db.student.insertOne( //here, student is a collection(Similar to MYSQL table)
         "marks": 56
     }
 );
-
-db.student.insertMany( // it takes an array of objects
+// 'db' refers to that database which we are using, through use() command
+// in db.student.insertOne(), if student collection exist in db then it will store in that collection otherwise it creates a new collection in our database
+db.student.insertMany( // it takes an array of objects(json)
     [
         {
             "name": "Bob",
@@ -33,5 +39,6 @@ db.student.insertMany( // it takes an array of objects
         }
     ]
 );
-db.student.find() //to display all data in the collection
+
+db.student.find(); //to display all data in the collection
 
