60a0a0999dd2492d414d06761bcd61b35293f48c
ranjeetkumar4941 <ranjeetkumar49424@gmail.com>
2026-01-19 16:52:03 +0530

    allchat&createchat route added

--- Backend/8. MongoDB with Express/index.js
+++ Backend/8. MongoDB with Express/index.js
@@ -5,10 +5,12 @@ const path = require('path');
 const port = 8080;
 
 const Chat = require('./models/chat.js');
+const { log } = require('console');
 
 // setting view engine
 app.set("view engine", 'ejs');
 app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({extended: true}));
 
 // connecting with MongoDB
 main().then(() => {
@@ -21,9 +23,41 @@ async function main(){
     await mongoose.connect("mongodb://127.0.0.1:27017/whatsapp")
 }
 
-app.get("/", (req, res) => {
-    res.send("This is home");
+app.get("/chats", (req, res) => {
+    Chat.find({}).then((result) => {
+        res.render('allchats.ejs', {chats: result});
+        
+    }).catch((err) => {
+        console.log(err);
+        res.send("Error in Database...");
+        
+    })
+    // res.send("This is home");
 })
+
+app.get("/chats/new", (req, res) => {
+    res.render("newchat.ejs");
+})
+
+app.post("/chats", (req, res) => {
+    console.log(req.body);
+    let newchat = new Chat({
+        from: req.body.from,
+        to: req.body.to,
+        msg: req.body.message,
+        created_at: new Date()
+    })
+
+    newchat.save().then(() => {
+        console.log("Chat was saved.");
+        
+    }).catch((err) => {
+        console.log(err);
+        res.send("Error in DB");
+    })
+    res.redirect("/chats");
+})
+
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
     
